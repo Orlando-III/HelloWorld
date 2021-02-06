4c2af0ec99ab6dae8cef1ded0b8baace4aab6cd3
Orlando-III <78619394+Orlando-III@users.noreply.github.com>
2021-02-05 19:00:46 -0800

    Update HelloWorld.py

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,1 +1,9 @@
-print (" Fat Meguru Milkies")
+import numpy as np
+import nn
+import nnfs
+from nnfs.datasets import spiral_data
+
+X = np.random.randn(1, 5)
+
+net = nn.Network([5, 5, 5, 5, 5, 3])
+print(net.forward_prop(X))
